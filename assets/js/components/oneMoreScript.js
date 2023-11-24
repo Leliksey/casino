6d1d1b5722e3a69c389d40c97679168ba17f6ad2
Maxopenstudio <71130450+Maxopenstudio@users.noreply.github.com>
2023-11-24 17:07:51 +0200

    third commit

--- assets/js/components/oneMoreScript.js
+++ assets/js/components/oneMoreScript.js
@@ -3,11 +3,13 @@ $(document).ready(function() {
         $(this).hide();
         $(".burger__icon_close").show();
         $(".burger__block").addClass("open");
+        $("html, body").addClass("ovh");
     })
     $(".burger__icon_close").click(function() {
         $(this).hide();
         $(".burger__icon").show();
         $(".burger__block").removeClass("open");
+        $("html, body").removeClass("ovh");
     });
 
     $('.cards').owlCarousel({
