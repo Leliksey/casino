42f838798219723b60671bd7260ba0cdf2f7b068
Maxopenstudio <71130450+Maxopenstudio@users.noreply.github.com>
2023-11-27 17:03:22 +0200

    почти все

--- assets/js/components/oneMoreScript.js
+++ assets/js/components/oneMoreScript.js
@@ -4,12 +4,15 @@ $(document).ready(function() {
         $(".burger__icon_close").show();
         $(".burger__block").addClass("open");
         $("html, body").addClass("ovh");
-    })
+    });
     $(".burger__icon_close").click(function() {
         $(this).hide();
         $(".burger__icon").show();
         $(".burger__block").removeClass("open");
         $("html, body").removeClass("ovh");
+    });
+    $(".menu__link").click(function() {
+        $(".burger__icon_close").click();
     });
 
     $('.cards').owlCarousel({
@@ -74,6 +77,61 @@ $(document).ready(function() {
                     },
                     1400:{
                         items:6
+                    }
+                }
+            });
+        }
+    });
+    $(".pageTemplate .news").owlCarousel({
+        loop:true,
+        nav:true,
+        mouseDrag:true,
+        touchDrag: true,
+        pullDrag:false,
+        freeDrag:false,
+        URLhashListener:false,
+        dots:true,
+        autoplay:false,
+        margin: 20,
+        responsive:{
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+    if($(window).width() < 769) {
+        $('.pageTemplate .news').owlCarousel('destroy')
+    };
+    $(window).on("resize", function() {
+        if($(window).width() < 769) {
+            $('.pageTemplate .news').owlCarousel('destroy')
+        } else {
+            $('.pageTemplate .news').owlCarousel({
+                loop:true,
+                nav:true,
+                mouseDrag:true,
+                touchDrag: true,
+                pullDrag:false,
+                freeDrag:false,
+                URLhashListener:false,
+                dots:true,
+                autoplay:false,
+                margin: 20,
+                responsive:{
+                    0:{
+                        items:1
+                    },
+                    768:{
+                        items:2
+                    },
+                    992:{
+                        items:3
                     }
                 }
             });
